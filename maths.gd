5c7fe9378086b9919fc1d18f6e7e15302aaa18df
Robin Six <six.robin@hotmail.fr>
2024-07-30 13:03:39 +0200

    Added point_left_to_segment func in maths

--- maths.gd
+++ maths.gd
@@ -4,19 +4,27 @@ extends RefCounted
 static func clampi01(value: int) -> int:
 	return clamp(value, 0, 1)
 
-
 static func clampf01(value: float) -> float:
 	return clamp(value, 0.0, 1.0)
 
 
-static func greatest_common_divisor(a: int, b: int) -> int:
+static func remap01(value: float, istart: float, istop: float) -> float:
+	return remap(value, istart, istop, 0.0, 1.0)
+
+static func remap_clamped(value: float, istart: float, istop: float, ostart: float, ostop: float) -> float:
+	return clamp(remap(value, istart, istop, ostart, ostop), ostart, ostop)
+
+static func remap01_clamped(value: float, istart: float, istop: float) -> float:
+	return clamp(remap(value, istart, istop, 0.0, 1.0), 0.0, 1.0)
+
+
+static func gcd(a: int, b: int) -> int:
 	while b != 0:
 		var c: int = a % b
 		a = b
 		b = c
 
 	return abs(a)
-
 
 static func factorial(value: int) -> int:
 	var factorial: int = 1
@@ -29,13 +37,11 @@ static func factorial(value: int) -> int:
 	return factorial
 
 
-static func remap01(value: float, istart: float, istop: float) -> float:
-	return remap(value, istart, istop, 0.0, 1.0)
-
-
-static func remap_clamped(value: float, istart: float, istop: float, ostart: float, ostop: float) -> float:
-	return clamp(remap(value, istart, istop, ostart, ostop), ostart, ostop)
-
-
-static func remap01_clamped(value: float, istart: float, istop: float) -> float:
-	return clamp(remap(value, istart, istop, 0.0, 1.0), 0.0, 1.0)
+static func point_left_to_segment(a: Vector2, b: Vector2, p: Vector2) -> float:
+	var f := (b.y - a.y) * (p.x - a.x) - (p.y - a.y) * (b.x - a.x)
+	if f > 0.0:
+		return 1.0
+	elif f < 0.0:
+		return -1.0
+	else:
+		return 0.0
