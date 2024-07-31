f625e2aadd887452b6e4c11c3044cd9060086a1d
Robin Six <six.robin@hotmail.fr>
2024-07-31 12:31:38 +0200

    Added RSResources (float, int, string)

---
+++ scripts/resources/res_int.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name RSInt
+
+@export var value: int
+
+
+func _init(p_value: int = 0):
+	self.value = p_value
---
+++ scripts/resources/res_string.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name RSString
+
+@export var value: String
+
+
+func _init(p_value: String = ""):
+	self.value = p_value
