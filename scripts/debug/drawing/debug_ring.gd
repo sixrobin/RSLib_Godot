d8060361bb7beaa5d0bdbb98a9c456c6c84fa528
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-08-13 22:03:46 +0200

    Update debug_ring.gd

--- scripts/debug/drawing/debug_ring.gd
+++ scripts/debug/drawing/debug_ring.gd
@@ -18,6 +18,5 @@ func _init(center: Vector2, radius1: float, radius2: float, resolution: int):
 func draw(drawer: CanvasItem):
 	var circle1 = DebugCircle.new(self._center, self._radius1, self._resolution).set_color(self._color).set_width(self._width)
 	var circle2 = DebugCircle.new(self._center, self._radius2, self._resolution).set_color(self._color).set_width(self._width)
-	
 	circle1.draw(drawer)
 	circle2.draw(drawer)
