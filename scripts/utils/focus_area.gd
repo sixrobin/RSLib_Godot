935aa2364118f56cfd1cd0c7284fcdb3ba1c8ef8
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-08-15 19:29:16 +0200

    Focus area focuses on ready

--- scripts/utils/focus_area.gd
+++ scripts/utils/focus_area.gd
@@ -8,9 +8,17 @@ extends Node2D
 
 func _ready():
 	self.top_level = true
+	self.global_position = self._target.global_position
 
 
 func _process(delta: float):
+	self.focus_target()
+	
+	RSDraw.marker(self._target).set_color(self._debug_color).set_always_draw(true)
+	RSDraw.rect(self, _size).set_color(self._debug_color).set_always_draw(true)
+
+
+func focus_target():
 	var half_size := self._size * 0.5
 	var offset := self._target.global_position - self.global_position
 	
@@ -23,6 +31,3 @@ func _process(delta: float):
 		self.global_position.y += offset.y - half_size.y
 	elif offset.y < -half_size.y:
 		self.global_position.y += offset.y + half_size.y
-		
-	RSDraw.marker(self._target).set_color(self._debug_color).set_width(2).set_always_draw(true)
-	RSDraw.rect(self, _size).set_color(self._debug_color).set_width(2).set_always_draw(true)
