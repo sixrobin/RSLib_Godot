249f1c22913db7f05a36b0d17cbbbf07f602e2f4
Robin Six <six.robin@hotmail.fr>
2024-08-20 16:38:25 +0200

    Values debugger can now draw texts at any position

--- scripts/debug/values_debugger.gd
+++ scripts/debug/values_debugger.gd
@@ -1,8 +1,11 @@
 extends Node
 
+const COLOR: Color = Color.YELLOW
+
 var _values_text: String = ""
 var _label: Label
 var _key_just_pressed: bool = false
+var _positioned_texts = {}
 
 
 func _ready():
@@ -10,7 +13,7 @@ func _ready():
 	
 	self._label = Label.new()
 	self._label.z_index = 2^63 - 1
-	self._label.modulate = Color(1, 1, 0, 1)
+	self._label.modulate = self.COLOR
 	self._label.visible = false
 	
 	var label_settings := LabelSettings.new()
@@ -35,12 +38,37 @@ func _process(delta: float):
 	
 	self._label.text = self._values_text
 	self._values_text = ""
-
+	
+	var positioned_texts_keys := self._positioned_texts.keys()
+	for key in positioned_texts_keys:
+		self._positioned_texts[key] += 1
+		if self._positioned_texts[key] == 2:  # Buffer to avoid destroying label right after its spawn.
+			self._positioned_texts.erase(key)
+			key.queue_free()
+		
 
 func format(key, value) -> String:
 	const FORMAT: String = "%s: %s"
 	return (FORMAT + "\n") % [str(key), str(value)]
 
 
-func debug_value(key, value):
-	self._values_text += self.format(key, value)
+func debug_value(key, value, position: Vector2 = Vector2.INF):
+	var debug_text: String = self.format(key, value)
+	
+	if not position.is_finite():
+		self._values_text += debug_text
+	elif self._label.visible:
+		var positioned_label = self.create_positioned_text(debug_text, position)
+		self._positioned_texts[positioned_label] = 0
+
+
+func create_positioned_text(debug_text: String, position: Vector2) -> Label:
+	var positioned_label = Label.new()
+	positioned_label.z_index = 2^63 - 1
+	positioned_label.modulate = self.COLOR
+	positioned_label.size.x = 1000.0
+	positioned_label.global_position = position
+	positioned_label.text = debug_text
+	
+	self.add_child(positioned_label)
+	return positioned_label
