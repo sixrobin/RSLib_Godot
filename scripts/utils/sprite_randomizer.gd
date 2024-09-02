822ae56ceed11e8d86aba4b79252a58add65bee9
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-09-02 13:41:50 +0200

    Added random color and offset to sprite randomizer

--- scripts/utils/sprite_randomizer.gd
+++ scripts/utils/sprite_randomizer.gd
@@ -5,6 +5,8 @@ extends Node
 @export var randomize_flip_h: bool = false
 @export var randomize_flip_v: bool = false
 @export var random_scale: Vector2 = Vector2.ONE
+@export var random_color: Gradient = null
+@export var random_offset_max: float = 0.0
 
 
 func _ready():
@@ -13,18 +15,21 @@ func _ready():
 
 func randomize_sprite():
 	var sprite2D = self.get_parent() as Sprite2D
-	
 	if not sprite2D:
 		print("Invalid parent node to randomize sprite!")
 		return
-		
+	
 	if self.textures != null and self.textures.size() > 0:
 		sprite2D.texture = self.textures.pick_random()
-		
+	
 	if self.randomize_flip_h:
 		sprite2D.flip_h = randf() < 0.5
-		
+	
 	if self.randomize_flip_v:
 		sprite2D.flip_v = randf() < 0.5
-		
-	sprite2D.scale *= randf_range(random_scale.x, random_scale.y)
+	
+	if self.random_color:
+		sprite2D.modulate = self.random_color.sample(randf())
+	
+	sprite2D.scale *= randf_range(self.random_scale.x, self.random_scale.y)
+	sprite2D.global_position += Vector2.RIGHT.rotated(randf() * TAU) * randf() * self.random_offset_max
