7003ffbf9be961ada08dff2f2bf17a503c1e706b
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-11-02 15:42:42 +0100

    Added sprite flip randomization using scale

--- scripts/utils/sprite_randomizer.gd
+++ scripts/utils/sprite_randomizer.gd
@@ -4,6 +4,8 @@ extends Node
 @export var textures: Array[Texture2D]
 @export var randomize_flip_h: bool = false
 @export var randomize_flip_v: bool = false
+@export var randomize_flipscale_x: bool = false
+@export var randomize_flipscale_y: bool = false
 @export var random_scale: Vector2 = Vector2.ONE
 @export var random_color: Gradient = null
 @export var random_offset_max: float = 0.0
@@ -32,4 +34,9 @@ func randomize_sprite():
 		sprite2D.modulate = self.random_color.sample(randf())
 	
 	sprite2D.scale *= randf_range(self.random_scale.x, self.random_scale.y)
+	if self.randomize_flipscale_x:
+		sprite2D.scale.x *= 1 if randf() < 0.5 else -1
+	if self.randomize_flipscale_y:
+		sprite2D.scale.y *= 1 if randf() < 0.5 else -1
+		
 	sprite2D.global_position += Vector2.RIGHT.rotated(randf() * TAU) * randf() * self.random_offset_max
