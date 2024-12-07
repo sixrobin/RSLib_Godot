eaa32d0b69b5df123bb0b9678a27b2d0401babea
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-12-07 16:58:09 +0100

    Command panel category system

--- scripts/debug/command_panel/command_panel.gd
+++ scripts/debug/command_panel/command_panel.gd
@@ -6,6 +6,7 @@ const WIDTH: int = 256
 
 var _canvas_layer: CanvasLayer = null
 var _buttons_container: Control = null
+var _categories: Dictionary = {}  # Container per category ID
 var _commands: Array[PanelCommand] = []
 var _sources: Dictionary = {}  # Commands count per source
 
@@ -34,6 +35,7 @@ func create_panel():
 	var screen_resolution: Vector2 = get_viewport().get_visible_rect().size
 	
 	self._canvas_layer = CanvasLayer.new()
+	self._canvas_layer.layer = 1000
 	self.add_child(self._canvas_layer)
 	
 	var control: Control = Control.new()
@@ -41,7 +43,7 @@ func create_panel():
 	self._canvas_layer.add_child(control)
 	
 	var background: ColorRect = ColorRect.new()
-	background.color = Color(0.1, 0.1, 0.1, 0.7)
+	background.color = Color(0.1, 0.1, 0.1, 0.8)
 	background.set_anchors_preset(Control.PRESET_RIGHT_WIDE)
 	background.size = Vector2(WIDTH, screen_resolution.y - MARGIN * 2)
 	background.position = Vector2(screen_resolution.x - WIDTH - MARGIN, MARGIN)
@@ -53,20 +55,27 @@ func create_panel():
 	background.add_child(scroll_container)
 	
 	var vbox: VBoxContainer = VBoxContainer.new()
+	vbox.add_theme_constant_override("separation", 0)
 	vbox.set_anchors_preset(Control.PRESET_FULL_RECT)
 	vbox.custom_minimum_size = Vector2(WIDTH, 0)
 	scroll_container.add_child(vbox)
 	self._buttons_container = vbox
 	
 	#for i in 100:
-		#self.add(self, "Test %s" % [i], func(): print("Test %s" % [i]))
+		#self.add(self, "Test", "Test %s" % [i], func(): print("Test %s" % [i]))
 
 
-func add(source: Node, action_name: String, action: Callable, key: int = -1):
+func add(source: Node, category: String, action_name: String, action: Callable, key: int = -1):
 	var command: PanelCommand = PanelCommand.new(source, action_name, action, key)
 	self._commands.append(command)
 	
-	var button: BaseButton = self.add_button(command.label, key)
+	if category == "":
+		category = "General"
+	
+	if not self._categories.has(category):
+		self.add_category(category)
+	
+	var button: BaseButton = self.add_button(category, command.label, key)
 	button.button_down.connect(command.execute)
 	command.set_button(button)
 	
@@ -92,9 +101,28 @@ func on_source_tree_exited(source: Node):
 		remove(command)
 
 
-func add_button(text: String, key: int = -1) -> BaseButton:
+func add_category(id: String) -> VBoxContainer:
+	var vbox: VBoxContainer = VBoxContainer.new()
+	vbox.add_theme_constant_override("separation", 0)
+	
+	var category_label: Label = Label.new()
+	category_label.text = id.to_upper()
+	category_label.self_modulate = Color.YELLOW
+	category_label.vertical_alignment = VERTICAL_ALIGNMENT_BOTTOM
+	category_label.add_theme_font_size_override("font_size", 12)
+	vbox.add_child(category_label)
+	
+	var spacing: Control = Control.new()
+	spacing.custom_minimum_size.y = 12
+	vbox.add_child(spacing)
+	
+	self._categories[id] = vbox
+	self._buttons_container.add_child(vbox)
+	return vbox
+
+
+func add_button(category: String, text: String, key: int = -1) -> BaseButton:
 	var button: Button = Button.new()
-	self._buttons_container.add_child(button)
 	button.custom_minimum_size.y = 16
 	
 	var label: Label = Label.new()
@@ -102,5 +130,9 @@ func add_button(text: String, key: int = -1) -> BaseButton:
 	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 	label.add_theme_font_size_override("font_size", 12)
 	button.add_child(label)
+	
+	var category_container := self._categories[category] as VBoxContainer
+	category_container.add_child(button)
+	category_container.move_child(button, category_container.get_child_count() - 2)
 	
 	return button
