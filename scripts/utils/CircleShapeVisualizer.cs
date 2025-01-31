079929e7e702bfb4b48a66717e0ea2388c944918
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-01-31 15:59:46 +0100

    C# migration WIP

---
+++ scripts/utils/CircleShapeVisualizer.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+public partial class CircleShapeVisualizer : CollisionShape2D
+{
+    [Export] private Color _color = Colors.Magenta;
+
+    public override void _Process(double delta) {
+        base._Process(delta);
+
+        if (Shape is CircleShape2D circleShape2D) {
+            RSLib.GE.Debug.Debugger.Drawer?.Circle(this, circleShape2D.Radius * GlobalScale.X).SetColor(_color);
+        }
+    }
+}
--- scripts/utils/circle_shape_visualizer.gd
+++
@@ -1,8 +0,0 @@
-extends CollisionShape2D
-
-@export var _color: Color = Color.MAGENTA
-
-
-func _process(delta: float):
-	if self.shape is CircleShape2D:
-		RSDraw.circle(self, self.shape.radius * self.global_scale.x).set_color(self._color)
