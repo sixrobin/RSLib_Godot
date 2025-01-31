011a9f7d6431d8742a58fa89bbb2b76372d07baf
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-01-31 22:52:07 +0100

    Added CustomComparers class

---
+++ scripts/utils/CustomComparers.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+public static class CustomComparers
+{
+    public static bool IsClosestTo(Node2D source, Node2D a, Node2D b) {
+        return (a.GlobalPosition - source.GlobalPosition).LengthSquared() < (b.GlobalPosition - source.GlobalPosition).LengthSquared();
+    }
+}
