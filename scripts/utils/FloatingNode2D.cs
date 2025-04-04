e213805e627ca54a438a46706286796d468431c8
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-04-04 18:01:18 +0200

    Create FloatingNode2D.cs

---
+++ scripts/utils/FloatingNode2D.cs
@@ -0,0 +1,31 @@
+using Godot;
+using RSLib.GE;
+
+public partial class FloatingNode2D : Node2D
+{
+    [Export] private float _amplitude = 30f;
+    [Export] private float _speed = 3f;
+
+    private Node2D _movedNode;
+    private float _initY;
+    private float _timer;
+    
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        _movedNode = GetParent<Node2D>();
+        _initY = _movedNode.Position.Y;
+        _timer = GD.Randf() * Mathf.Pi;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        
+        _timer += (float)delta * _speed;
+
+        float wave = Mathf.Sin(_timer) * _amplitude;
+        _movedNode.Position = _movedNode.Position.WithY(_initY + wave);
+    }
+}
