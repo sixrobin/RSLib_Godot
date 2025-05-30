1ecaac6b89ce3f01be3e46eef2b7b455c592b818
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-05-30 19:16:02 +0200

    AudioHandler first draft

---
+++ scripts/audio/AudioHandler.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+public partial class AudioHandler : Node2D
+{
+    private const string SFX_BUS_NAME = "sfx";
+
+    public AudioHandler(string sfxParentFolder)
+    {
+        _sfxParentFolder = sfxParentFolder;
+        
+        AudioServer.AddBus(AudioServer.GetBusCount());
+        _sfxBusID = AudioServer.GetBusCount() - 1;
+        AudioServer.SetBusName(_sfxBusID, SFX_BUS_NAME);
+    }
+
+    private readonly string _sfxParentFolder;
+    private readonly int _sfxBusID;
+
+    public void SetSFXVolume(float volume)
+    {
+        AudioServer.SetBusVolumeLinear(_sfxBusID, volume);
+    }
+    
+    public void SFX(string path, SFXArgs args = null)
+    {
+        string streamPath = $"{_sfxParentFolder}/{path}.wav";
+        
+        AudioStreamPlayer2D audioPlayer2D = new()
+        {
+            Stream = GD.Load<AudioStream>(streamPath),
+            Bus = SFX_BUS_NAME,
+            GlobalPosition = args?.GlobalPosition ?? GlobalPosition,
+            VolumeLinear = args?.Volume ?? 1f,
+        };
+        
+        AddChild(audioPlayer2D);
+
+        audioPlayer2D.Finished += audioPlayer2D.QueueFree;
+        audioPlayer2D.Play();
+    }
+
+    public class SFXArgs
+    {
+        public Vector2? GlobalPosition = null;
+        public float Volume = 1f;
+    }
+}
