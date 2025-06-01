40a4262cf5195956a544d3f78af9cce9e18e397d
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-06-01 17:57:18 +0200

    Update TimerExtensions.cs

--- scripts/extensions/TimerExtensions.cs
+++ scripts/extensions/TimerExtensions.cs
@@ -9,9 +9,9 @@ namespace RSLib.GE
         /// </summary>
         /// <param name="timer">Timer to get the completion percentage of.</param>
         /// <returns>Completion percentage in [0,1] clamped range.</returns>
-        public static double GetCompletionPercentage(this Timer timer)
+        public static float GetCompletionPercentage(this Timer timer)
         {
-            return Mathf.Clamp(1 - timer.TimeLeft / timer.WaitTime, 0, 1);
+            return (float)Mathf.Clamp(1f - timer.TimeLeft / timer.WaitTime, 0, 1);
         }
     }
 }
