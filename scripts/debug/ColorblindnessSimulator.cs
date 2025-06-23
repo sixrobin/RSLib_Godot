ae3341fde61a24363570b84ecb774c58cfb0e818
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-06-23 22:40:49 +0200

    Quick refactoring

--- scripts/debug/ColorblindnessSimulator.cs
+++ scripts/debug/ColorblindnessSimulator.cs
@@ -9,14 +9,14 @@ namespace RSLib.GE.Debug
             Name = nameof(ColorblindnessSimulator);
             Layer = 128;
 
-            _material = new ShaderMaterial
+            ShaderMaterial material = new()
             {
                 Shader = GD.Load<Shader>("res://externals/RSLib_Godot/shaders/debug/colorblind_filter.gdshader"),
             };
             
             ColorRect filterRect = new()
             {
-                Material = _material,
+                Material = material,
                 MouseFilter = Control.MouseFilterEnum.Ignore,
             };
             filterRect.SetAnchorsPreset(Control.LayoutPreset.FullRect);
@@ -25,26 +25,24 @@ namespace RSLib.GE.Debug
             Debugger.CommandPanel.Add(this, "colorblindness", "none", () => _material.SetShaderParameter("severity", 0f));
             Debugger.CommandPanel.Add(this, "colorblindness", "protanomaly", () =>
             {
-                _material.SetShaderParameter("mode", 0);
-                _material.SetShaderParameter("severity", 1f);
+                material.SetShaderParameter("mode", 0);
+                material.SetShaderParameter("severity", 1f);
             });
             Debugger.CommandPanel.Add(this, "colorblindness", "deuteranomaly", () =>
             {
-                _material.SetShaderParameter("mode", 1);
-                _material.SetShaderParameter("severity", 1f);
+                material.SetShaderParameter("mode", 1);
+                material.SetShaderParameter("severity", 1f);
             });
             Debugger.CommandPanel.Add(this, "colorblindness", "tritanomaly", () =>
             {
-                _material.SetShaderParameter("mode", 2);
-                _material.SetShaderParameter("severity", 1f);
+                material.SetShaderParameter("mode", 2);
+                material.SetShaderParameter("severity", 1f);
             });
             Debugger.CommandPanel.Add(this, "colorblindness", "achromatopsia", () =>
             {
-                _material.SetShaderParameter("mode", 3);
-                _material.SetShaderParameter("severity", 1f);
+                material.SetShaderParameter("mode", 3);
+                material.SetShaderParameter("severity", 1f);
             });
         }
-        
-        private ShaderMaterial _material;
     }
 }
