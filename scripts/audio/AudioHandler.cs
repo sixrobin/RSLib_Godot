1d691c04be11a5138e945770e4acb40bd047e846
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-06-23 22:56:24 +0200

    Removed toggle sfx debug

--- scripts/audio/AudioHandler.cs
+++ scripts/audio/AudioHandler.cs
@@ -1,13 +1,9 @@
 using Godot;
-using RSLib.GE.Debug;
 
 public partial class AudioHandler : Node2D
 {
     public const string SFX_BUS_NAME = "sfx";
 
-    private float _sfxVolume;
-    private bool _debugMuteSFX;
-    
     public AudioHandler(string sfxParentFolder)
     {
         _sfxParentFolder = sfxParentFolder;
@@ -15,12 +11,6 @@ public partial class AudioHandler : Node2D
         AudioServer.AddBus(AudioServer.GetBusCount());
         _sfxBusID = AudioServer.GetBusCount() - 1;
         AudioServer.SetBusName(_sfxBusID, SFX_BUS_NAME);
-        
-        Debugger.CommandPanel.Add(this, "audio", "toggle sfx", () =>
-        {
-            _debugMuteSFX = !_debugMuteSFX;
-            SetSFXVolume(_sfxVolume);
-        });
     }
 
     private readonly string _sfxParentFolder;
@@ -28,8 +18,7 @@ public partial class AudioHandler : Node2D
 
     public void SetSFXVolume(float volume)
     {
-        _sfxVolume = volume;
-        AudioServer.SetBusVolumeLinear(_sfxBusID, _debugMuteSFX ? 0f : _sfxVolume);
+        AudioServer.SetBusVolumeLinear(_sfxBusID, volume);
     }
     
     public void SFX(string path, SFXArgs args = null)
