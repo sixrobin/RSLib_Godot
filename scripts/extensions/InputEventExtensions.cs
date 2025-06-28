f8c7207f2557cc6eb44e992eda7cb70c18f0eabc
Robin Six <55784799+sixrobin@users.noreply.github.com>
2025-06-28 19:23:31 +0200

    Create InputEventExtensions.cs

---
+++ scripts/extensions/InputEventExtensions.cs
@@ -0,0 +1,19 @@
+namespace RSLib.GE
+{
+    using Godot;
+
+    public static class InputEventExtensions
+    {
+        public static string SimplifiedName(this InputEvent evt)
+        {
+            return evt switch
+            {
+                InputEventKey inputEventKey                   => inputEventKey.PhysicalKeycode.ToString(),
+                InputEventMouseButton inputEventMouseButton   => inputEventMouseButton.ButtonIndex.ToString(),
+                InputEventJoypadButton inputEventJoypadButton => inputEventJoypadButton.ButtonIndex.ToString(),
+                InputEventJoypadMotion inputEventJoypadMotion => inputEventJoypadMotion.Axis.ToString(),
+                _                                             => evt.AsText()
+            };
+        }
+    }
+}
