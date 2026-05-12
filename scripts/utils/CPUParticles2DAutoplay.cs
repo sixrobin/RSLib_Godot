017250fbd5778f0057776bc0866c556c3b8d3093
Robin Six <six.robin@hotmail.fr>
2026-05-12 23:04:35 +0200

    Particles autoplay one frame delay

--- scripts/utils/CPUParticles2DAutoplay.cs
+++ scripts/utils/CPUParticles2DAutoplay.cs
@@ -2,9 +2,14 @@ using Godot;
 
 public partial class CPUParticles2DAutoplay : CpuParticles2D
 {
-    public override void _Ready()
+    public async override void _Ready()
     {
         base._Ready();
+
+        // Wait for one frame to play to ensure particles are positioned before being drawn.
+        Visible = false;
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+        Visible = true;
         Restart();
     }
 }
--- scripts/utils/GPUParticles2DAutoplay.cs
+++ scripts/utils/GPUParticles2DAutoplay.cs
@@ -2,9 +2,14 @@ using Godot;
 
 public partial class GPUParticles2DAutoplay : GpuParticles2D
 {
-    public override void _Ready()
+    public async override void _Ready()
     {
         base._Ready();
+        
+        // Wait for one frame to play to ensure particles are positioned before being drawn.
+        Visible = false;
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+        Visible = true;
         Restart();
     }
 }
