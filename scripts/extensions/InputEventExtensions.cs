7906ce1421381cff01b4a5b95df20bd9015ad33c
Robin Six <six.robin@hotmail.fr>
2026-06-21 23:33:52 +0200

    Added InputEvent.LocalizedName extension

--- scripts/extensions/InputEventExtensions.cs
+++ scripts/extensions/InputEventExtensions.cs
@@ -8,11 +8,25 @@ namespace RSLib.GE
         {
             return evt switch
             {
-                InputEventKey inputEventKey                   => DisplayServer.KeyboardGetKeycodeFromPhysical(inputEventKey.PhysicalKeycode).ToString(),
-                InputEventMouseButton inputEventMouseButton   => inputEventMouseButton.ButtonIndex.ToString(),
-                InputEventJoypadButton inputEventJoypadButton => inputEventJoypadButton.ButtonIndex.ToString(),
-                InputEventJoypadMotion inputEventJoypadMotion => inputEventJoypadMotion.Axis.ToString(),
-                _                                             => evt.AsText()
+                InputEventKey key                   => DisplayServer.KeyboardGetKeycodeFromPhysical(key.PhysicalKeycode).ToString(),
+                InputEventMouseButton mouseButton   => mouseButton.ButtonIndex.ToString(),
+                InputEventMouseMotion mouseMotion   => mouseMotion.ButtonMask.ToString(),
+                InputEventJoypadButton joypadButton => joypadButton.ButtonIndex.ToString(),
+                InputEventJoypadMotion joypadMotion => joypadMotion.Axis.ToString(),
+                _                                   => evt.AsText(),
+            };
+        }
+        
+        public static string LocalizedName(this InputEvent inputEvent)
+        {
+            return inputEvent switch
+            {
+                InputEventKey key                   => Localizer.TryGet($"key_{DisplayServer.KeyboardGetKeycodeFromPhysical(key.PhysicalKeycode).ToString().ToLower()}", out string result) ? result : inputEvent.SimplifiedName(),
+                InputEventMouseButton mouseButton   => Localizer.TryGet($"mouse_button_{mouseButton.ButtonIndex.ToString().ToLower()}", out string result) ? result : inputEvent.SimplifiedName(),
+                InputEventMouseMotion mouseMotion   => Localizer.TryGet($"mouse_motion_{mouseMotion.ButtonMask.ToString().ToLower()}", out string result) ? result : inputEvent.SimplifiedName(),
+                InputEventJoypadButton joypadButton => Localizer.TryGet($"joypad_button_{joypadButton.ButtonIndex.ToString().ToLower()}", out string result) ? result : inputEvent.SimplifiedName(),
+                InputEventJoypadMotion joypadMotion => Localizer.TryGet($"joypad_motion_{joypadMotion.Axis.ToString().ToLower()}", out string result) ? result : inputEvent.SimplifiedName(),
+                _                                   => inputEvent.SimplifiedName(),
             };
         }
     }
