4264c04789049a70ba96d1ec6b8f0dd04f447dd9
Robin Six <55784799+sixrobin@users.noreply.github.com>
2026-07-26 15:31:54 +0200

    Second order dynamics util

---
+++ scripts/utils/SecondOrderDynamics.cs
@@ -0,0 +1,42 @@
+﻿namespace RSLib.GE
+{
+    using Godot;
+
+    public class SecondOrderDynamics
+    {
+        public SecondOrderDynamics(float frequency, float damping, float responsiveness, Vector2 initPosition)
+        {
+            _k1 = damping / (Mathf.Pi * frequency);
+            _k2 = 1f / ((Mathf.Pi * frequency * 2f) * (Mathf.Pi * frequency * 2f));
+            _k3 = responsiveness * damping / (Mathf.Pi * frequency * 2f);
+            
+            _xp = initPosition;
+            _y = initPosition;
+            _yd = Vector2.Zero;
+        }
+        
+        private readonly float _k1;
+        private readonly float _k2;
+        private readonly float _k3;
+        
+        private Vector2 _xp;
+        private Vector2 _y;
+        private Vector2 _yd;
+
+        public Vector2 Update(float time, Vector2 to, Vector2? velocity = null)
+        {
+            // Velocity estimation.
+            if (velocity == null)
+            {
+                velocity = (to - _xp) / time;
+                _xp = to;
+            }
+
+            float k2Stable = Mathf.Max(_k2, 1.1f * (time * time / 4f + time * _k1 / 2f));
+            _y += time * _yd;
+            _yd += time * (to + _k3 * velocity.Value - _y - _k1 * _yd) / k2Stable;
+
+            return _y;
+        }
+    }
+}
